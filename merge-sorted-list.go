34f44e4f86e40328442b66eb880ed26ef197fa0a
elvindu <dumanxiang@wxblockchain.com>
2020-05-06 11:24:22 +0800

    add merge sorted list

--- merge-sorted-list.go
+++ merge-sorted-list.go
@@ -1,32 +1,38 @@
 //剑指offer 合并两个已经排序的列表
 package algorithm_lab
 
+import "math"
+
 func MergeSortedList(l1, l2 *List) *List {
 	l1node, l2node := l1.Head, l2.Head
+	prel1 := NewListNode(math.MinInt64)
+	prel1.Next = l1node
 
-	for ; !IsNil(l1node) && !IsNil(l2node); {
-		if l1node.Value.(int) > l2node.Value.(int) {
-			l1node = l1node.Next
-		} else {
-			l2Next := l2node.Next
-			l1Next := l1node.Next
+	for ; !IsNil(l2node); {
+		for ; !IsNil(l1node); {
+			if l1node.Value.(int) < l2node.Value.(int) {
+				prel1 = l1node
+				l1node = l1node.Next
+			} else {
+				tmpL2Next := l2node.Next
 
-			l1node.Next = l2node
-			l2node.Next = l1Next
+				prel1.Next = l2node
+				prel1.Next.Next = l1node
+				prel1 = prel1.Next
 
-			l2node = l2Next
-			l1node = l1Next
+				l2node = tmpL2Next
+
+				break
+			}
 		}
 
-		if IsNil(l1node) || IsNil(l2node) || IsNil(l1node.Next) || IsNil(l2node.Next) {
+		if IsNil(l1node) {
 			break
 		}
 	}
 
-	if IsNil(l1node.Next) {
-		if !IsNil(l2node) {
-			l1node.Next = l2node
-		}
+	if !IsNil(l2node) {
+		prel1.Next = l2node
 	}
 
 	return l1

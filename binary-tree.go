c5fdf9136a7ab6d73aded503489f6aa440cafd7b
elvindu <dumanxiang@wxblockchain.com>
2020-05-06 18:34:18 +0800

    add serialize and deserialize

--- binary-tree.go
+++ binary-tree.go
@@ -58,6 +58,7 @@ func (bt *BinaryTree) BFS() string {
 	return strings.Join(strArry, ",")
 }
 
+//剑指offer 按行打印每层数据
 func (bt *BinaryTree) BFS2() [][]interface{} {
 	ret := [][]interface{}{}
 	queue := NewQueue()
@@ -82,6 +83,67 @@ func (bt *BinaryTree) BFS2() [][]interface{} {
 	}
 
 	return ret
+}
+
+func SerializeByRecursion(root *BinaryTreeNode) []string {
+	strArr := []string{}
+	if IsNil(root) {
+		strArr = append(strArr, "$")
+		return strArr
+	}
+
+	strArr = append(strArr, fmt.Sprintf("%v", root.Value))
+
+	strArr = append(strArr, SerializeByRecursion(root.Left)...)
+	strArr = append(strArr, SerializeByRecursion(root.Right)...)
+
+	return strArr
+}
+
+//非递归版本的前序遍历
+func (bt *BinaryTree) Serialize() string {
+	stack := NewStack(1000)
+	node := bt.Root
+	ret := []string{}
+	for {
+		if !IsNil(node) {
+			ret = append(ret, fmt.Sprintf("%v", node.Value))
+			stack.Push(node)
+			node = node.Left
+		} else {
+			ret = append(ret, "$")
+			e := stack.Pop().(*BinaryTreeNode)
+			node = e.Right
+		}
+		if stack.Size() == 0 && IsNil(node) {
+			break
+		}
+	}
+
+	return strings.Join(ret, ",")
+}
+
+var index = 0
+
+func Deserialize(str []string) *BinaryTreeNode {
+	if len(str) == 0 {
+		return nil
+	}
+	if index >= len(str) {
+		return nil
+	}
+
+	if str[index] == "$" {
+		index++
+		return nil
+	}
+
+	root := NewBinaryTreeNode(str[index])
+	index++
+	root.Left = Deserialize(str)
+	root.Right = Deserialize(str)
+
+	return root
 }
 
 //默认是宽度优先遍历
--- binary-tree_test.go
+++ binary-tree_test.go
@@ -2,6 +2,7 @@ package algorithm_lab
 
 import (
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
@@ -41,4 +42,45 @@ func TestBinaryTree_BFS2(t *testing.T) {
 			[]interface{}{2, 3},
 			[]interface{}{4, 5, 6, 7},
 		}, bt.BFS2())
+}
+
+func TestBinaryTree_Serialize(t *testing.T) {
+	bt := NewBinaryTree()
+	root := NewBinaryTreeNode(1)
+	root.SetLeft(NewBinaryTreeNode(2))
+	root.SetRight(NewBinaryTreeNode(3))
+
+	root.Left.SetLeft(NewBinaryTreeNode(4))
+
+	root.Right.SetLeft(NewBinaryTreeNode(6))
+	root.Right.SetRight(NewBinaryTreeNode(7))
+	bt.Root = root
+
+	assert.Equal(t, "1,2,4,$,$,$,3,6,$,$,7,$", bt.Serialize())
+}
+
+func TestDeserialize(t *testing.T) {
+	str := []string{"1", "2", "4", "$", "$", "$", "3", "6", "$", "$", "7", "$", "$"}
+	//str := "1,2,4,$,$,$,3,6,$,$,7,$,$"
+	root := Deserialize(str)
+	bt := NewBinaryTree()
+	bt.Root = root
+
+	t.Log(bt.Serialize())
+}
+
+//todo 递归比非递归版本最后会多一个$
+func TestSerializeByRecursion(t *testing.T) {
+	bt := NewBinaryTree()
+	root := NewBinaryTreeNode(1)
+	root.SetLeft(NewBinaryTreeNode(2))
+	root.SetRight(NewBinaryTreeNode(3))
+
+	root.Left.SetLeft(NewBinaryTreeNode(4))
+
+	root.Right.SetLeft(NewBinaryTreeNode(6))
+	root.Right.SetRight(NewBinaryTreeNode(7))
+	bt.Root = root
+
+	assert.Equal(t, "1,2,4,$,$,$,3,6,$,$,7,$,$", strings.Join(SerializeByRecursion(bt.Root), ","))
 }

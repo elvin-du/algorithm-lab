6939bae44e46b2fa186637d4cd3b325600204f27
elvindu <dumanxiang@wxblockchain.com>
2020-05-08 18:14:35 +0800

    add comment

--- get-median-from-stream.go
+++ get-median-from-stream.go
@@ -5,13 +5,15 @@ var (
 	maxHeap []int
 	minHeap []int
 )
-
+//确保小根堆里面的数 > 大根堆里面的数
 func InsertStreamData(number int) {
 	if len(maxHeap) == len(minHeap) {
+		//先从大顶堆过滤一遍,用来确保小顶堆里面的数是比大顶堆里面的数大
 		maxHeap = InsertMaxHeap(maxHeap, number)
 		max := MaxHeapPoll(&maxHeap)
 		minHeap = InsertMinHeap(minHeap, max)
 	} else {
+		//先从小顶堆过滤一遍,用来确保大顶堆里面的数是比小顶堆里面的数小
 		minHeap = InsertMinHeap(minHeap, number)
 		min := MinHeapPoll(&minHeap)
 		maxHeap = InsertMaxHeap(maxHeap, min)
